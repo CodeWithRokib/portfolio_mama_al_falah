0d9114fe04371bfc263d3416ec0460af6c60f3e5
Rokibul Hasan Rokib <rokibulhasan018722@gmail.com>
2024-05-24 15:12:33 +0600

    home and about frontend add

---
+++ resources/views/frontend/layout/css.blade.php
@@ -0,0 +1,10 @@
+
+  <link rel="stylesheet" href="{{asset('assets/vendor/bootstrap/css/bootstrap.min.css')}}">
+  <link rel="stylesheet" href="{{asset('assets/vendor/bootstrap-icons/bootstrap-icons.css')}}">
+  <link rel="stylesheet" href="{{asset('assets/vendor/fontawesome-free/css/all.min.css')}}">
+  <link rel="stylesheet" href="{{asset('assets/vendor/glightbox/css/glightbox.min.css')}}">
+  <link rel="stylesheet" href="{{asset('assets/vendor/swiper/swiper-bundle.min.css')}}">
+  <link rel="stylesheet" href="{{asset('assets/vendor/aos/aos.css')}}">
+
+    <!-- Template Main CSS File -->
+    <link href="{{asset('assets/css/main.css')}}" rel="stylesheet">
---
+++ resources/views/frontend/master.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <!-- Favicons -->
+
+  <link href="{{asset('assets/img/favicon.png')}}" rel="icon">
+  <link href="{{asset('assets/img/apple-touch-icon.png')}}" rel="apple-touch-icon">
+      <!-- Google Fonts -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,600;1,700&family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Inter:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
+    <title>@yield('title')</title>
+
+    @include('frontend.layout.css')
+</head>
+<body>
+
+    @include('frontend.layout.header')
+
+    @yield('content')
+
+    @include('frontend.layout.footer')
+    
+    @include('frontend.layout.js')
+</body>
+</html>
